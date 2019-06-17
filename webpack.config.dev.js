3aa68d40aaa87262e777f035a25fae3f49389da4
bank8426 <bank_55555@hotmail.com>
2019-06-17 11:55:50 +0700

    add webpack dev config,set IMAGES_PATH

---
+++ webpack.config.dev.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+const IMAGES_PATH = process.env.IMAGES_PATH || '/images/';
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: 'build',
+  },
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    inline: true,
+    host: '0.0.0.0',
+    port: 3000,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    contentBase: 'public',
+  },
+
+  plugins: [
+    // This makes it possible for us to safely use env vars on our code
+    new webpack.DefinePlugin({
+      'process.env.IMAGES_PATH': JSON.stringify(IMAGES_PATH),
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
+
+module.exports = config;
