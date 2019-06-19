c37ca0f41e3c0f345e7e4f72a21cee0f47d20f06
bank8426 <bank_55555@hotmail.com>
2019-06-19 18:52:35 +0700

    update style Card, CardPayment

--- src/components/Charity/Card.js
+++ src/components/Charity/Card.js
@@ -1,17 +1,48 @@
-import React from 'react'
+import React,{useState} from 'react'
 import styled from 'styled-components';
 import CardPayment from './CardPayment';
 const StyledCard = styled.div`
-  margin: 10px;
-  border: 1px solid #ccc;
+  margin: 0 10px 50px;
+  background: #FFFFFF;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 10px 10px 10px 10px;
+  position: relative;
 `;
 
-const Card = ({item,handlePay}) => {
+const StyledImg = styled.div`
+  width: 650px;
+  overflow: hidden;
+  background-image: url(${props => props.src});
+  height: 300px;
+  border-radius: 10px 10px 0px 0px;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+`;
+
+const StyledH2 = styled.h2`
+  margin: 1em 0em 1em 1em;
+`;
+
+const StyledButton = styled.button`
+  margin: auto 1em auto 0;
+`;
+
+const StyledNameLabel = styled.div`
+  display: grid;
+  grid-template-columns: 1fr max-content;
+`;
+
+const Card = ({item}) => {
+  const [displayPayment, setDisplayPayment] = useState(false)
   return (
     <StyledCard>
-      <p>{item.name}</p>
-      <img src={process.env.IMAGES_PATH + item.image} />
-      <CardPayment index={item.id} handlePay={handlePay} currency={item.currency}/>
+      <StyledImg src={process.env.IMAGES_PATH + item.image}/>
+      <StyledNameLabel>
+        <StyledH2>{item.name}</StyledH2>
+        <StyledButton className='primary' onClick={() => setDisplayPayment(true) }>Donate</StyledButton>
+      </StyledNameLabel>
+      { displayPayment ? <CardPayment index={item.id} currency={item.currency} onClose={() => setDisplayPayment(false)}/> : null }
     </StyledCard>
   )
 }
--- src/components/Charity/CardPayment.js
+++ src/components/Charity/CardPayment.js
@@ -3,10 +3,51 @@ import { connect } from 'react-redux';
 import styled from 'styled-components';
 import * as paymentActions from '../../redux/actions/paymentActions';
 import { bindActionCreators } from 'redux'
+import RadioButton from '../common/RadioButton';
 const StyledCardPayment = styled.div`
-
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255,255,255,0.95);
+  padding: 15% 20px;
+  box-sizing: border-box;
+  border-radius: 10px;
 `;
-const CardPayment = ({index,savePayment,currency}) => {
+
+const StyledPaymentList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5,1fr);
+  padding: 0 100px;
+`;
+
+const StyledHeader = styled.h2`
+  text-align: center;
+`;
+
+const StyledCloseButton = styled.span`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  border-radius: 50%;
+  cursor: pointer;
+  font-size: 20px;
+  padding: 1px 8px;
+  color: #1b53f2;
+  font-weight: bold;
+
+  &:hover{
+    color: white;
+    background-color: #1B95F2;
+    border-color: #0062cc;
+  }
+`;
+
+const StyledPayButton = styled.button`
+  display: block;
+  margin: 2em auto;
+`;
+const CardPayment = ({index,savePayment,currency,onClose}) => {
   const payments = [10, 20, 50, 100, 500];
   const [selectedAmount, setSelectedAmount] = useState(payments[0])
   const handlePay = (id, amount, currency) => {
@@ -16,17 +57,19 @@ const CardPayment = ({index,savePayment,currency}) => {
   }
   return (
     <StyledCardPayment>
-      {
-        payments.map((amount, j) => (
-          <label key={j}>
-            <input
-              type="radio"
-              name={`payment${index}`}
-              onClick={ () => {setSelectedAmount(amount)}
-              } /> {amount}
-          </label>))
-      }
-      <button onClick={() => handlePay(index, selectedAmount, currency)}>Pay</button>
+      <StyledCloseButton onClick={() => onClose()}>X</StyledCloseButton>
+      <StyledHeader>Select the amount to donate ({currency})</StyledHeader>
+      <StyledPaymentList>
+        {
+          payments.map((amount, j) => (
+            <RadioButton key={j}
+              name={`payment${index}`} 
+              handleClick={() => {setSelectedAmount(amount)}} 
+              displayMessage={amount} checked={amount===selectedAmount}/>
+          ))
+        }
+      </StyledPaymentList>
+      <StyledPayButton className='primary' onClick={() => handlePay(index, selectedAmount, currency)}>Pay</StyledPayButton>
     </StyledCardPayment>
   )
 }
