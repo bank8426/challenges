f48e0c27aeb235dd6e9c07121708f449d548fb5a
bank8426 <bank_55555@hotmail.com>
2019-06-19 12:13:39 +0700

    update css for Header, Message

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import styled from 'styled-components';
 const StyledHeader = styled.h1`
-
+  background-color: #1B95F2;
+  margin: 0;
+  padding: 10px;
+  color: white;
+  text-align:center;
 `;
 const Header = () => {
   return (
--- src/components/common/Message.js
+++ src/components/common/Message.js
@@ -1,19 +1,37 @@
 import React from 'react'
 import styled from 'styled-components';
 
+const StyledContainer = styled.div`
+  position: fixed;
+  right: 0;
+  top: 0;
+`;
+
 const StyledMessage = styled.p`
-  color: red;
-  margin: 1em 0;
+  color: white;
+  margin: 1em 1em 0;
   font-weight: bold;
   font-size: 16px;
   text-align: center;
+  background: #2F80ED;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.25);
+  padding:10px;
+  &:nth-child(n+2) {
+    margin-top: 0em;
+  }
 `;
 
 const Message = ({message}) => {
   return (
-    <StyledMessage>
-      {message}
-    </StyledMessage>
+    <StyledContainer>
+      { message 
+        ? 
+        <StyledMessage>
+          {message}
+        </StyledMessage> : null
+      }
+    </StyledContainer>
+
   )
 }
 
