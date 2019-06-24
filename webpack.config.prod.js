7f73770d43f9755e84ed4cbe5c96a0002b6be15d
bank8426 <bank_55555@hotmail.com>
2019-06-25 00:28:20 +0700

    update webpack for development,production

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,35 +1,30 @@
 const webpack = require('webpack');
 const path = require('path');
-const IMAGES_PATH = process.env.IMAGES_PATH || '/images/';
-const API_PATH = process.env.API_PATH || 'http://localhost:3001';
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpackBundleAnalyzer = require('webpack-bundle-analyzer');
+const IMAGES_PATH = process.env.IMAGES_PATH || 'https://serene-goldberg-d32da8.netlify.com/images/';
+const API_PATH = process.env.API_PATH || 'https://db-heroku-for-challenge.herokuapp.com';
 const CopyPlugin = require('copy-webpack-plugin');
 
+process.env.NODE_ENV = 'production';
 const config = {
-  mode: 'development',
+  mode: 'production',
+  target: 'web',
+  devtool: 'source-map',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
     publicPath: '',
     filename: 'js/[name].js',
   },
-
-  devtool: 'inline-source-map',
-
-  devServer: {
-    inline: true,
-    host: '0.0.0.0',
-    port: 3000,
-    historyApiFallback: true,
-    disableHostCheck: true,
-  },
-
   plugins: [
+    // Display bundle stats
+    new webpackBundleAnalyzer.BundleAnalyzerPlugin({ analyzerMode: 'static' }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Tamboon Development',
-      template: path.resolve(__dirname, 'public', 'index.html'),
+      title: 'Tamboon Production',
+      template: 'public/index.html',
       favicon: 'public/favicon.ico',
     }),
     // This makes it possible for us to safely use env vars on our code
